9dab277da926f83e340a8d82d0b0fdf16c44040c
Wladimir <wladimir.code@gmail.com>
2020-07-19 17:23:17 -0300

    add: App logo

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Image } from 'react-native';
 
 import { Container } from './styles';
 
+import logoImg from '../../assets/logo.png';
+
 const SignIn: React.FunctionComponent = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Image source={logoImg} />
+    </Container>
+  );
 };
 
 export default SignIn;
