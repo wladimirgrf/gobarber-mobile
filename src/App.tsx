a6d54faa97f56b53c00f2e3e8cda3598b20aaf5f
Wladimir <wladimir.code@gmail.com>
2020-07-19 16:36:14 -0300

    feat: StatusBar style

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StatusBar } from 'react-native';
 
-const App: React.FunctionComponent = () => <View />;
+const App: React.FunctionComponent = () => (
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <View style={{ flex: 1, backgroundColor: '#312e38' }} />
+  </>
+);
 
 export default App;
