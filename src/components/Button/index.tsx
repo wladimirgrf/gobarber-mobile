4e9e283d46a390f9a84b48c584081439c6a3fd86
Wladimir <wladimir.code@gmail.com>
2020-07-21 21:22:03 -0300

    add: Components Input & Button, styles

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { RectButtonProperties } from 'react-native-gesture-handler';
+
+import { Container, ButtonText } from './styles';
+
+interface ButtonProps extends RectButtonProperties {
+  children: string;
+}
+
+const Button: React.FunctionComponent<ButtonProps> = ({
+  children,
+  ...rest
+}) => (
+  <Container {...rest}>
+    <ButtonText>{children}</ButtonText>
+  </Container>
+);
+
+export default Button;
