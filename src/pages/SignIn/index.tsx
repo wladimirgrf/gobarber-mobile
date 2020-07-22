75972a71a1480ef0deca7c46671b4726745856f7
Wladimir <wladimir.code@gmail.com>
2020-07-21 21:23:14 -0300

    feat: SignIn with new components (Input, Button)

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container, Title } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FunctionComponent = () => {
   return (
     <Container>
       <Image source={logoImg} />
       <Title>Login</Title>
+
+      <Input name="email" icon="mail" placeholder="Email" />
+      <Input name="password" icon="lock" placeholder="Password" />
+
+      <Button
+        onPress={() => {
+          console.log('teste');
+        }}
+      >
+        Login
+      </Button>
     </Container>
   );
 };
