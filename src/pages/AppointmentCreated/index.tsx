2c5d733dfbc63497ea987c01bf476ead8f047274
Wladimir <wladimir.code@gmail.com>
2020-10-12 11:20:27 -0300

    add: new Pages, Profile, CreateAppointment and AppointmentCreated

---
+++ src/pages/AppointmentCreated/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const AppointmentCreated: React.FunctionComponent = () => {
+  return <View />;
+};
+
+export default AppointmentCreated;
---
+++ src/pages/CreateAppointment/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const CreateAppointment: React.FunctionComponent = () => {
+  return <View />;
+};
+
+export default CreateAppointment;
