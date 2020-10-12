83e7a03368149beb35368c016e1c63b089262d1d
Wladimir <wladimir.code@gmail.com>
2020-10-12 14:20:25 -0300

    add: User interface, Auth hook

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -9,6 +9,13 @@ import AsyncStorage from '@react-native-community/async-storage';
 
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  avatarUrl: string;
+}
+
 interface SignInCredentials {
   email: string;
   password: string;
@@ -16,11 +23,11 @@ interface SignInCredentials {
 
 interface AuthState {
   token: string;
-  user: unknown;
+  user: User;
 }
 
 interface AuthContextData {
-  user: unknown;
+  user: User;
   loading: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
